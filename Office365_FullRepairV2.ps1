b75e7f717b694a64b58ffa265acf36f159d8f709
tgeske88 <tgeske88@gmail.com>
2022-02-22 09:35:38 -0600

    Create Office365_FullRepairV2.ps1

---
+++ Office365_FullRepairV2.ps1
@@ -0,0 +1,13 @@
+$command64 = @' 
+cmd.exe /C "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeClickToRun.exe" scenario=Repair platform=x64 culture=en-us RepairType=FullRepair forceappshutdown=True DisplayLevel=False
+'@
+
+$command86 = @' 
+cmd.exe /C "C:\Program Files\Microsoft Office 15\ClientX86\OfficeClickToRun.exe" scenario=Repair platform=x86 culture=en-us RepairType=FullRepair forceappshutdown=True DisplayLevel=False
+'@
+
+if(Test-Path -Path "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeClickToRun.exe"){
+    Invoke-Expression -Command:$command64
+} elseif(Test-PAth -Path "C:\Program Files\Microsoft Office 15\ClientX32\OfficeClickToRun.exe"){
+    Invoke-Expression -Command:$command86
+}
